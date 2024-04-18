cd6af9d7bbc1594d6353948a5269228c010b5998
szalkai.patricia <szalkai.patricia@T15-01>
2024-04-18 09:12:10 +0200

    Add project files.

---
+++ Eredmeny.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EredmenyekCLI
+{
+    class Eredmeny
+    {
+            public int Id { get; set; }
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+            public string Targy { get; set; }
+            public int Szazalek { get; set; }
+            public static int Eredmenyy
+            {
+            get
+            {
+                if (Szazalek <= 24)
+                {
+                    return 1;
+                }
+                else if (Szazalek <= 39)
+                {
+                    return 2;
+                }
+                else if (Szazalek <= 59)
+                {
+                    return 3;
+                }
+                else if (Szazalek <= 79)
+                {
+                    return 4;
+                }
+                else
+                {
+                    return 5;
+                }
+            }; set { };
+              }
+
+            public override string ToString()
+            {
+                return $"Eredmeny <{Firstname} {Lastname} : {Eredmenyy}>";
+            }
+
+            public static Eredmeny CreateFromLine(string line, char separator = ',')
+            {
+                string[] values = line.Split(separator);
+
+                return new Eredmeny()
+                {
+                    Id = int.Parse(values[0]),
+                    Firstname = values[1],
+                    Lastname = values[2],
+                    Targy = values[3],
+                    Szazalek = int.Parse(values[4]),
+                    Eredmeny = int.Parse(values[5])
+                };
+            }
+        }
+}
---
+++ EredmenyekRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EredmenyekCLI
+{
+    class EredmenyekRepository
+    {
+
+            public static string Path { get; set; }
+
+            public static bool skipHeader { get; set; }
+            public static char Separator { get; set; } = ',';
+            public static List<Eredmeny> FindAll()
+            {
+                using (StreamReader reader = new StreamReader(Path))
+                {
+                    if (skipHeader)
+                    {
+                        reader.ReadLine(); //fejléc eldobása
+                    }
+                    List<Eredmeny> people = new List<Eredmeny>();
+
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                    Eredmeny person = Eredmeny.CreateFromLine(line, Separator);
+                        people.Add(person);
+                    }
+                    return people;
+                }
+            }
+
+            public static List<Eredmeny> FindAllBySearch(string firstName, string lastname, string targy, int szazalek, int eredmeny)
+            {
+                List<Eredmeny> people = new List<Eredmeny>();
+                foreach (Eredmeny person in FindAll())
+                {
+                    if (!person.Firstname.ToLower().Contains(firstName.ToLower().Trim()))
+                    {
+                        continue;
+                    }
+
+                    else if (!person.Lastname.ToLower().Contains(lastname.ToLower().Trim()))
+                    {
+                        continue;
+                    }
+                    /*else if (!person.Targy.ToLower().Contains(email.ToLower().Trim()))
+                    {
+                        continue;
+                    }
+                    else if (person.Szazalek)
+                    {
+                        continue;
+                    }
+                    else if (Eredmeny != "" && person.Eredmeny !=)
+                    {
+                        continue;
+                    }
+
+                    people.Add(person);
+                }*/
+
+                return people;
+            }
+        }
+
